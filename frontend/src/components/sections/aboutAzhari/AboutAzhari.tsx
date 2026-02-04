7e18f588ec879b1c4a48b5a31bdce9f5b77b807d
AlanNieves <alannieves233@gmail.com>
2026-02-04 12:15:31 -0600

    aboutAzhari temporal template to be fixed and merged

---
+++ frontend/src/components/sections/aboutAzhari/AboutAzhari.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import AboutImage from './AboutImage';
+import AboutText from './AboutText';
+
+export default function AboutAzhari() {
+  return (
+    <section
+      id="about-azhari"
+      className="relative w-full bg-[#1c1c1c] py-24 overflow-hidden"
+    >
+      {/* CONTENIDO SUPERIOR */}
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-14 items-start">
+          {/* TÍTULO IZQUIERDA */}
+          <div>
+
+            <h1 className="text-[38px] md:text-[42px]
+            font-normal
+            tracking-[-0.01em]
+            text-white/90">
+              Torre Azhari
+            </h1>
+          </div>
+
+          {/* TEXTO DERECHA */}
+          <AboutText />
+        </div>
+      </div>
+
+      {/* IMAGEN FULL WIDTH */}
+      <div className="mt-20">
+        <AboutImage />
+      </div>
+    </section>
+  );
+}
